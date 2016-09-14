65903edc1728cdde5952107c8edb67b45122f6b1
Evfrosiniya <evfrosiniya.z@gmail.ru>
2016-09-14 16:33:52 +0300

    add filter and tests

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
+
 let express = require('express');
-let technologger = require('technologger');
 let parser = require('body-parser');
 let app = express();
 let technoDoc = require('techno-gendoc');
@@ -7,12 +7,27 @@ let path = require('path');
 
 let technolibs = require('technolibs');
 
+// let emailDb = {};
+// app.post('/users', (req, res, body) => {
+//     console.log(req.body);
+//
+//     if (emailDb[req.body.email]) {
+//         emailDb[req.body.email]++;
+//     }
+//     else {
+//         emailDb[req.body.email] = 1;
+//     }
+//     console.log(emailDb[req.body.email]);
+//     res.send(''+emailDb[req.body.email]);
+//     // TODO: вернуть количество обращений
+// });
+
+
 app.use('/', express.static('public'));
 technoDoc.generate(require('./api'), 'public');
 
 app.use(parser.json());
 app.use('/libs', express.static('node_modules'));
-app.use(technologger);
 
 app.post('/api/messages', (req, res) => {
 	technolibs.publish(req.body).then(body => res.json(req.body));
@@ -25,20 +40,6 @@ app.get('/api/messages', function (req, res) {
 		technoDoc.mock(require('./api/scheme/Message')),
 		technoDoc.mock(require('./api/scheme/Message'))
 	])
-//app.use(technologger);
-let emailDb = {};
-app.post('/users', (req, res, body) => {
-    console.log(req.body);
-
-    if (emailDb[req.body.email]) {
-        emailDb[req.body.email]++;
-    }
-    else {
-        emailDb[req.body.email] = 1;
-    }
-    console.log(emailDb[req.body.email]);
-    res.send(''+emailDb[req.body.email]);
-    // TODO: вернуть количество обращений
 });
 
 app.listen(process.env.PORT || 3000, () => {
