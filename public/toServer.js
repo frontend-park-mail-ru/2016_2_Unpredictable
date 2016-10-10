e9f25c5977f429ac15d63b8ee18fd3f22f6876c8
IlyaNikk <vic096@yandex.ru>
2016-10-10 17:57:43 +0300

    create toServer function and add it

---
+++ public/toServer.js
@@ -0,0 +1,50 @@
+(function () {
+	'use strict';
+
+	function sendToServer(params) {
+
+		const fetch = window.fetch;
+
+		return fetch('https://morning-hamlet-29496.herokuapp.com/' + params.url, {
+			method: params.method,
+			body: JSON.stringify(params.body),
+			mode: 'cors',
+			headers: {
+				'Content-type': 'application/json; charset=UTF-8'
+			}
+		}).then(function (resp) {
+			if (resp.status < 300) {
+				return resp.json();
+			}
+			return Promise.reject(resp.json());
+		}).then(function (answer) {
+			
+			console.log(answer);
+			debugger;
+			params.attrs.forEach(name => {
+				window.localStorage.setItem(name.toLowerCase(), answer[name]);
+			});
+			if(params.oneMore === true) {
+				let newParams = {
+					url: 'api/sessions',
+					method: 'POST',
+					attrs: ['sessionid'],
+					body: params.body,
+					oneMore: false
+				};
+				sendToServer(newParams);
+			}
+			else
+				return {};
+		}).catch(function (data) {
+			try {
+				this._errorText._get().innerText = obj.error || 'Неизвестная ошибка. Попробуйте позже';
+			} catch (_) {
+				this._errorText._get().innerText = 'Неизвестная ошибка. Попробуйте позже';
+			}
+			return Promise.reject();
+		}.bind(this));
+	}
+
+	window.sendToServer = sendToServer;
+})();
