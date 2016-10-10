ad2a67577baf66bc15fbcec37c3b77bd8ce804b3
IlyaNikk <vic096@yandex.ru>
2016-10-10 23:31:02 +0300

    add router and views

--- public/main.js
+++ public/main.js
@@ -1,9 +1,16 @@
 (function () {
 	'use strict';
 
-	const Application = window.Application;
+	const Router = window.Router;
+	const AppView = window.AppView;
+	const SignView = window.SignView;
 
-	const app = new Application();
-	app.renderTo(document.body);
+
+	// TIP: роуты нужно указывать от наиболее специфичного к наименее специфичному
+	// З.Ы. чтобы более ранние роуты не были префиксами более поздних ;]
+	(new Router)
+		.addRoute('/logout', AppView)
+		.addRoute('/', SignView)
+		.start();
 
 })();
