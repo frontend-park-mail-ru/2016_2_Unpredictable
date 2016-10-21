6beff83cd18caf838583681916700d279d99e469
Evfrosiniya <evfrosiniya.z@gmail.ru>
2016-10-21 22:44:11 +0300

    test commit

--- karma.conf.js
+++ karma.conf.js
@@ -7,7 +7,7 @@ module.exports = function (config) {
 		frameworks: ['jasmine'],
 
 		files: [
-			'./public/components/**/*.js',
+			//'./public/components/**/*.js',
 			'./public/modules/**/*.js',
 			'./public/views/**/*.js',
 			'./test/**/*.spec.js'
