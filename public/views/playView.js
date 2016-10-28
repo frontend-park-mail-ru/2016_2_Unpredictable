d744d288b8fffb3064abd28a9010231681e8436e
Evfrosiniya <evfrosiniya.z@gmail.ru>
2016-10-29 02:28:47 +0300

    Предзащита

--- public/views/playView.js
+++ public/views/playView.js
@@ -11,6 +11,10 @@
 
 		init() {
 			this._el.innerHTML = '<img src=http://naklejka.ru/image/cache/data/naklejki/stickerbombing/stiker-s-kotom-persik-450x450.png>';
+			this._el.addEventListener('click', () => {
+				window.location.href = 'https://share.proto.io/GBTQEN/';
+			});
+			this._el.style = 'cursor:pointer;';
 		}
 	}
 
